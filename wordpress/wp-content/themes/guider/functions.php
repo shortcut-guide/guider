d8cdaff42916b2c169dbdf8d97c8f892acee564a
dashio <dahio2015@gmail.com>
2017-03-26 18:47:29 +0900

    //固定ページの親スラッグ取得 //

--- wordpress/wp-content/themes/guider/functions.php
+++ wordpress/wp-content/themes/guider/functions.php
@@ -636,6 +636,18 @@ function wpautop_filter($content) {
 	return $content;
 }
 
+////////////////////////////////////
+//固定ページの親スラッグ取得 //
+////////////////////////////////////
+
+function is_parent_slug() {
+	global $post;
+	if ($post->post_parent) {
+		$post_data = get_post($post->post_parent);
+		return $post_data->post_name;
+	}
+}
+
 // ------------------------------
 //=add_actions
 // ------------------------------
